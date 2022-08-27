dd80bb74e259c8b61252b6b34c02ae855dffc0e1
onebitbool <onebitbool@gmail.com>
2022-08-27 14:54:39 +0800

    Add pingpong program

--- user/sleep.c
+++ user/sleep.c
@@ -6,7 +6,7 @@ int
 main(int argc, char **argv)
 {
     if(argc < 2){
-        fprintf(2, "usage: sleep tick...\n");
+        fprintf(2, "usage: sleep <time>\n");
         exit(1);
     }
     sleep(atoi(argv[2]));
